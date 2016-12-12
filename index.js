2c344fa5b0d5608d08f2061cd31c4a81c6f91129
Vitor Camacho <vitor.camachoo@gmail.com>
2016-12-12 22:38:52 +0000

    Remove console.log

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV == 'development') {
-  console.log(process.env.NODE_ENV);
   require('babel-register');
   require('./src/builder').start()
 } else {
